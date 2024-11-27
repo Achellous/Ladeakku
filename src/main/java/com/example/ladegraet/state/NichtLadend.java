5ae5999cb45f8c8eff11bae3f55946d4d5f325e5
Fawaz Thomas Bouti <32436688+Achellous@users.noreply.github.com>
2024-11-27 12:04:22 +0100

    Initial commit

--- src/main/java/com/example/ladegraet/state/NichtLadend.java
+++ src/main/java/com/example/ladegraet/state/NichtLadend.java
@@ -12,7 +12,11 @@ public class NichtLadend extends Zustand {
 
     @Override
     public void bearbeiten(Ladegeraet ladegreat) {
-        System.out.println("Ladegerät ist nicht ladend. Zustand bleibt unverändert.");
+        if (ladegerät.getLadestand() < 20) {
+            System.out.println("Akkustand < 20%: Akku defekt. Zustand bleibt NichtLadend.");
+        } else if (ladegerät.getLadestand() >= 20 && ladegerät.getLadestand() < 100) {
+            System.out.println("Akkustand >= 20%: Wechsel zu NormalLadend.");
+            ladegerät.setZustand(NormalLadend.getLadend());
     }
 }
 
